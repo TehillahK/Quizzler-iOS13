8d769f5ebf491bbc75cc9213e9f52759915dec0a
Tehillahk <kangambt@myumanitoba.ca>
2025-01-20 02:23:11 -0600

    adding files

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -14,58 +14,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let questions = [
-        Question(q: "A slug's blood is green.", a: "True"),
-        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-        Question(q: "Google was originally called 'Backrub'.", a: "True"),
-        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-
-    ]
-    
-    var currentQuestion = 0
+    var quizLogic = QuizLogic()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        questionLabel.text = questions[currentQuestion].question
+        questionLabel.text = quizLogic.getCurrentQuestion()
         progressBar.progress = 0.0
     }
 
     @IBAction func answerGiven(_ sender: UIButton) {
         
         // if question is correct
-        if (sender.titleLabel?.text == "True"){
-            trueButton.backgroundColor = UIColor.green
+        let isCorrect = quizLogic.isQuestionCorrect((sender.titleLabel?.text!)!)
+        
+        if(isCorrect){
+            sender.backgroundColor = UIColor.green
         }else{
-            falseButton.backgroundColor = UIColor.red
+            sender.backgroundColor = UIColor.red
         }
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1 ) {
             UIView.animate(withDuration: 0.2) {
-                if (sender.titleLabel?.text == "True"){
-                    self.trueButton.backgroundColor = UIColor.clear
-                }else{
-                    self.falseButton.backgroundColor = UIColor.clear
-                }
+                sender.backgroundColor = UIColor.clear
             }
         }
         
         
         
-        if(currentQuestion < questions.count - 1){
-            currentQuestion += 1
-            questionLabel.text = questions[currentQuestion].question
-            progressBar.progress += Float(currentQuestion)/Float(questions.count)
-            
-        }
+      
+        updateUI()
+    }
+    
+    func updateUI() {
+        // go to next Question
+        quizLogic.nextQuestion()
+        
+        // get the new Question
+        questionLabel.text = quizLogic.getCurrentQuestion()
+        
+        //update progressbar
+        progressBar.progress += quizLogic.percentageProgress()
+    
+        
     }
     
 
